45745151bfba253bc9a96cd6ab3ec584c5616f4c
Amour SODJINOU <amoursodjinou@gmail.com>
2023-07-24 16:02:40 +0100

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccordionModule } from 'primeng/accordion';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    PanelModule,
+    ButtonModule,
+    TableModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
